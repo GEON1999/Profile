bb41729f0ef892b9904518b02b3d4cb58e4876cb
GEON1999 <phgst12@gmail.com>
2022-12-19 23:00:24 +0900

    HTML&CSS : Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Contact from "../components/contact";
 import Layout from "../components/layout";
 
 export default function Home() {
@@ -6,10 +7,12 @@ export default function Home() {
     <div>
       <Layout>
         <div className="text-center">
-          <div className="-mt-16 text-[28vw] font-extrabold text-transparent  bg-clip-text back bg-[url('../imgs/dark.gif')] -space-y-[7vw]">
-            <p>GEON</p>
-            <p className="text-[3vw] text-black font-bold">Creative.</p>
+          <div className="-mt-16 text-[25vw] font-extrabold text-transparent  bg-clip-text back bg-[url('../imgs/dark.gif')] flex flex-col">
+            <span>GEON</span>
           </div>
+          <span className="text-[3vw] font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-black to-gray-200">
+            Creative.
+          </span>
         </div>
       </Layout>
     </div>
