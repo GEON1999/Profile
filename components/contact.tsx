df3b685665d3638dbb8414a60e305712188fdf59
GEON1999 <phgst12@gmail.com>
2022-12-20 21:53:13 +0900

    reduce size for mobile

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,19 +2,18 @@ import Image from "next/image";
 
 export default function Contact() {
   return (
-    <div className="flex space-x-[10vw] justify-center items-center">
+    <div className="flex space-x-[6vw] justify-center items-center my-10">
       <div className="md:w-96">
-        <div className="space-y-3">
-          <span className="text-4xl font-extrabold">Feel Free to Contact</span>
+        <div className="space-y-2">
+          <span className="md:text-2xl text-lg font-extrabold">
+            Feel Free to Contact
+          </span>
           <p className="font-thin text-sm">
             저와 함께하고 싶으시다면, 연락해 주세요!!
           </p>
         </div>
-        <div className="flex justify-end -mt-5">
-          <Image src="/arrow.png" width="50" height="50" alt="arrow" />
-        </div>
       </div>
-      <div className="p-4 rounded-full bg-black text-white font-thin shadow-sm">
+      <div className="md:p-4 px-3 py-2 rounded-full bg-black text-white font-thin shadow-sm">
         CLICK
       </div>
     </div>
