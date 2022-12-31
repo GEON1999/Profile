38dec858ff5acef8e1553d9604496e6838bf1508
DESKTOP-SHB77O2\phgst <phgst12@gmail.com>
2022-12-31 14:53:16 +0900

    merge not to make any conflict

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
+import { useRef } from "react";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const scrollTopRef = useRef(null);
   return (
+
     <div className="w-full h-full">
       <Component {...pageProps} />
     </div>
+
   );
 }
 
