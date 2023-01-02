9d059fd3254892175abb0a7532131abce085dae2
DESKTOP-SHB77O2\phgst <phgst12@gmail.com>
2023-01-02 18:50:01 +0900

    work css

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,30 +1,13 @@
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import Contact from "../components/contact";
 import Layout from "../components/layout";
 import { cls } from "../libs/utils";
 import { motion, AnimatePresence, useInView } from "framer-motion";
 import WorkDetail from "../components/workDetail";
 
-const variants = {
-  toggled: {
-    opacity: 100,
-    zIndex: 1,
-  },
-  initial: {
-    opacity: 0,
-    zIndex: -10,
-    x: -100,
-  },
-  exit: {
-    opacity: 0,
-    zIndex: -10,
-  },
-};
-
 export default function Work() {
   const [toggleBox, setToggleBox] = useState<
-    "next" | "react" | "node" | "javascript" | null
+    "Next" | "React" | "Node" | "Js" | null
   >(null);
 
   useEffect(() => {
@@ -35,7 +18,7 @@ export default function Work() {
       <div>
         <Layout />
         <div className="felx justify-center text-center font-serif text-5xl mb-10 pt-40">
-          <span className="border-2 px-8 py-2 rounded-full border-black border-opacity-70 ">
+          <span className="border-b-2 px-8 py-2  border-black border-opacity-70 ">
             WORK
           </span>
         </div>
@@ -43,9 +26,9 @@ export default function Work() {
           <div className="text-2xl font-serif justify-items-start mb-14"></div>
           <div className="md:grid md:grid-cols-2 flex flex-col  gap-20">
             <motion.div
-              layoutId="next"
-              onClick={() => setToggleBox("next")}
-              className=" space-y-5 w-96"
+              layoutId="Next"
+              onClick={() => setToggleBox("Next")}
+              className=" space-y-5 w-96 cursor-pointer"
             >
               <div>
                 <div className="absolute border-b border-gray-300 w-96" />
@@ -87,9 +70,9 @@ export default function Work() {
               </div>
             </motion.div>
             <motion.div
-              layoutId="react"
-              className=" space-y-5 w-96"
-              onClick={() => setToggleBox("react")}
+              layoutId="React"
+              className=" space-y-5 w-96 cursor-pointer"
+              onClick={() => setToggleBox("React")}
             >
               <div>
                 <div className="absolute border-b border-gray-300 w-96" />
@@ -127,9 +110,9 @@ export default function Work() {
               </div>
             </motion.div>
             <motion.div
-              layoutId="node"
-              className=" space-y-5 w-96"
-              onClick={() => setToggleBox("node")}
+              layoutId="Node"
+              className=" space-y-5 w-96 cursor-pointer"
+              onClick={() => setToggleBox("Node")}
             >
               <div>
                 <div className="absolute border-b border-gray-300 w-96" />
@@ -141,7 +124,7 @@ export default function Work() {
               </div>
               <div className="grid grid-cols-2 items-start ml-5 gap-5">
                 <span className="">
-                  <h1 className="text-lg ">Javascript</h1>
+                  <h1 className="text-lg ">Js</h1>
                   <p className="text-sm text-gray-500">
                     to make web page interactive
                   </p>
@@ -171,16 +154,14 @@ export default function Work() {
               </div>
             </motion.div>
             <motion.div
-              layoutId="javascript"
-              className="space-y-5 w-96 transition duration-1000 ease-out hover:ease-in"
-              onClick={() => setToggleBox("javascript")}
+              layoutId="Js"
+              className="space-y-5 w-96 transition duration-1000 ease-out hover:ease-in cursor-pointer"
+              onClick={() => setToggleBox("Js")}
             >
               <div>
                 <div className="absolute border-b border-gray-300 w-96" />
                 <div className="relative -top-3 text-center ">
-                  <span className="text-xl font-serif bg-white px-3">
-                    Javascript
-                  </span>
+                  <span className="text-xl font-serif bg-white px-3">Js</span>
                 </div>
               </div>
               <div className="grid grid-cols-2  items-start  ml-5 gap-5">
@@ -202,7 +183,7 @@ export default function Work() {
       </div>
       <div
         className={cls(
-          "absolute w-screen h-screen bg-gray-800 bg-opacity-20 top-0 flex flex-col",
+          "fixed w-full h-full bg-gray-800 bg-opacity-20 top-0 bottom-0 left-0 right-0 ",
           toggleBox ? "z-40" : "hidden"
         )}
         onClick={() => setToggleBox(null)}
