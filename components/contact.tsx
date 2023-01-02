594037cb93d50efc1bdf06258930cd83dde687d6
DESKTOP-SHB77O2\phgst <phgst12@gmail.com>
2023-01-03 07:46:29 +0900

    contact component : hover animation

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
+import { motion, AnimatePresence, useInView } from "framer-motion";
 
 export default function Contact() {
   return (
     <div className="flex space-x-[6vw] justify-center items-center my-28">
-        <div className="space-y-2">
+      <div className="space-y-2">
         <div className="relative border-b-2 top-8 border-black w-full" />
-          <span className=" text-lg font-extrabold">
-            Contact
-          </span>
+        <span className=" text-lg font-extrabold">Contact</span>
 
-          <p className="font-thin text-sm ">
-            저와 함께하고 싶으시다면, 연락해 주세요.
-          </p>
-        </div>
+        <p className="font-thin text-sm ">
+          저와 함께하고 싶으시다면, 연락해 주세요.
+        </p>
+      </div>
       <Link href="/contact">
-        <div className="px-5 py-3 rounded-full text-sm font-thin shadow-sm border-black border ">
+        <a className="px-5 py-3 rounded-full text-sm font-thin shadow-sm border-black border hover:bg-black hover:text-white hover:ease-out duration-1000 ">
           CLICK
-        </div>
+        </a>
       </Link>
     </div>
   );
