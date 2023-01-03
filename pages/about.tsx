30905f6a88913abb3b4b6e3190a85b181ea30b5a
DESKTOP-SHB77O2\phgst <phgst12@gmail.com>
2023-01-03 17:40:41 +0900

    about : responsive size

--- pages/about.tsx
+++ pages/about.tsx
@@ -66,9 +66,9 @@ export default function About() {
   const ref = useRef(null);
   const ref2 = useRef(null);
   const ref3 = useRef(null);
-  const isInView = useInView(ref);
-  const isInView2 = useInView(ref2);
-  const isInView3 = useInView(ref3);
+  const isInView = useInView(ref, { once: true });
+  const isInView2 = useInView(ref2, { once: true });
+  const isInView3 = useInView(ref3, { once: true });
   useEffect(() => {
     if (isInView) {
       console.log("view!");
@@ -83,15 +83,20 @@ export default function About() {
             <span className="border-b-2 px-8 py-1  border-black border-opacity-70">
               ABOUT
             </span>
+            <div>
+              <span className=" text-base px-4 py-1 border-slate-400 text-slate-400">
+                scroll down to read more
+              </span>
+            </div>
           </div>
           <div className="md:mx-20 mx-4 ">
             <div className="">
-              <div className="space-y-8 mb-32 w-[40vw] flex justify-start flex-col font-light text-gray-600">
+              <div className="space-y-8 mb-32 lg:w-[40vw] flex justify-center lg:justify-start flex-col font-light text-gray-600">
                 <motion.h1
                   ref={ref}
                   variants={titleFadeIn}
                   initial={"initail"}
-                  animate={isInView ? "animate" : "initail"}
+                  animate={isInView ? "animate" : ""}
                   className="w-[40w] md:text-2xl text-lg font-mono text-black  "
                 >
                   Choice
@@ -99,21 +104,20 @@ export default function About() {
                 <motion.h2
                   variants={titleFadeIn}
                   initial={"initail"}
-                  animate={isInView ? "animate" : "initail"}
+                  animate={isInView ? "animate" : ""}
                   className="relative bottom-5 font-medium text-black"
                 >
-                  중요한 선택일수록 생각보단 행동을 합니다.
+                  생각보단 행동을 하려 합니다.
                 </motion.h2>
                 <motion.div
                   className="space-y-8"
                   variants={textFadeIn}
                   initial={"initail"}
-                  animate={isInView ? "animate" : "initail"}
+                  animate={isInView ? "animate" : ""}
                 >
                   <p>
-                    살아가다 보면 세상은 정해진 선택지와 틀 안에서의 반복 같고,
-                    옳고 그름이 정해져 있는 것만 같습니다. 그러나 선택지와 틀은
-                    아는 만큼 보이고, 옳고 그름의 기준도 개개인 마다 다릅니다.
+                    삶 안에서의 선택지들은 아는 만큼 보이고 과정과 결과는 온전히
+                    받아들여지기보단 상대적으로 판단됩니다.
                   </p>
                   <p>
                     그래서 저만의 기준이 필요했습니다. 상대적이지 않고 잘 변하지
@@ -136,20 +140,20 @@ export default function About() {
                   </p>
                 </motion.div>
               </div>
-              <div className="space-y-8 flex flex-col items-end font-light text-gray-600 mb-32">
+              <div className="space-y-8 flex flex-col lg:items-end font-light text-gray-600 mb-32">
                 <motion.h1
-                  className="w-[40w] md:text-2xl text-lg font-mono text-black"
+                  className="lg:w-[40w] md:text-2xl text-lg font-mono text-black"
                   ref={ref2}
                   variants={titleFadeIn2}
                   initial={"initail"}
-                  animate={isInView2 ? "animate" : "initail"}
+                  animate={isInView2 ? "animate" : ""}
                 >
                   Experience.
                 </motion.h1>
                 <motion.h2
                   variants={titleFadeIn2}
                   initial={"initail"}
-                  animate={isInView2 ? "animate" : "initail"}
+                  animate={isInView2 ? "animate" : ""}
                   className="relative bottom-5 font-medium text-black"
                 >
                   경험은 저에게 하는 투자라고 생각합니다.
@@ -158,36 +162,36 @@ export default function About() {
                   className="space-y-8 flex flex-col items-end"
                   variants={textFadeIn2}
                   initial={"initail"}
-                  animate={isInView2 ? "animate" : "initail"}
+                  animate={isInView2 ? "animate" : ""}
                 >
-                  <p className="w-[40vw]">
+                  <p className="lg:w-[40vw]">
                     저는 시야가 좁고 경험이 부족했기에 경험이 절실했습니다.
                     학교를 그만두고 무엇을 해야 할지 알 길이 없는 저는 시야를
                     넓히고 더 많은 경험을 하기 위해 필리핀 유학을 도전
                     해보았습니다. 그것이 저의 &#39;첫 도전&#39; 이였습니다.
                   </p>
-                  <p className="w-[40vw]">
+                  <p className="lg:w-[40vw]">
                     필리핀 스파르타 어학원에서는 아침 일과가 오전 6시부터 오후
                     9시까지 정해져 있고, 모든 게 통제되어 있었습니다. 무언가 한
                     가지를 위해 그렇게 많은 시간과 노력을 들인 게 처음이라 쉽지
                     않았습니다.
                   </p>
-                  <p className="w-[40vw]">
+                  <p className="lg:w-[40vw]">
                     선택에는 책임이 따른다는 걸 그때 깨달았던 것 같습니다.
                     하지만 노력과 결과에 대한 성취 또한 느껴보았습니다.
                   </p>
-                  <p className="w-[40vw]">
+                  <p className="lg:w-[40vw]">
                     이후 캐나다 유학, 컬리지 입학 그리고 회사 생활까지 많은
                     실패와 성취를 통해 경험했습니다.
                   </p>
                 </motion.div>
               </div>
-              <div className="space-y-8  w-[40vw] flex justify-start flex-col font-light text-gray-600">
+              <div className="space-y-8  lg:w-[40vw] flex justify-center lg:justify-start flex-col font-light text-gray-600">
                 <motion.h1
                   ref={ref3}
                   variants={titleFadeIn}
                   initial={"initail"}
-                  animate={isInView3 ? "animate" : "initail"}
+                  animate={isInView3 ? "animate" : ""}
                   className="w-[40w] md:text-2xl text-lg font-mono text-black"
                 >
                   High Goal.
@@ -196,7 +200,7 @@ export default function About() {
                   className="relative bottom-5 font-medium text-black"
                   variants={titleFadeIn}
                   initial={"initail"}
-                  animate={isInView3 ? "animate" : "initail"}
+                  animate={isInView3 ? "animate" : ""}
                 >
                   높이 향한 꿈은 저를 지속해서 앞으로 나아가게 해주는 원동력
                   입니다.
@@ -205,7 +209,7 @@ export default function About() {
                   className="space-y-8"
                   variants={textFadeIn}
                   initial={"initail"}
-                  animate={isInView3 ? "animate" : "initail"}
+                  animate={isInView3 ? "animate" : ""}
                 >
                   <p>
                     어릴 때는 그저 대단한 사람 되고 싶었습니다. 청소년이 되어
