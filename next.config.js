e16e7c9a141c41013b8babc32b8ecab067536263
DESKTOP-SHB77O2\phgst <phgst12@gmail.com>
2023-01-03 21:45:14 +0900

    publish error fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true },
+  experimental: {
+    externalDir:
+      true |
+      {
+        enabled: true,
+        silent: true,
+      },
+  },
   images: {
     remotePatterns: [
       {
