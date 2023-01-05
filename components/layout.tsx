3ea7c09289daa7e601872311fc4b75a06e754779
DESKTOP-SHB77O2\phgst <phgst12@gmail.com>
2023-01-05 15:09:29 +0900

    delete hover animation for mobile

--- components/layout.tsx
+++ components/layout.tsx
@@ -15,19 +15,19 @@ export default function Layout({ title, children }: LayoutProps) {
       <nav className=" md:text-xl md:pt-7 font-bold fixed w-[100%] z-10 bg-white pb-4">
         <div className="flex justify-between md:mx-20 mx-4 mt-5 md:mt-0">
           <Link href="/">
-            <div className="flex space-x-3 justify-center items-end">
+            <div className="group flex space-x-3 justify-center items-end">
               <div className="flex flex-col cursor-pointer">
-                <span className="text-5xl font-thin flex items-end text-gray-700 group -space-x-4">
-                  <p className="group-hover:mx-0.5 hover:ease-out duration-150">
+                <span className="text-5xl font-thin flex items-end text-gray-700  -space-x-4">
+                  <p className="md:group-hover:mx-0.5 md:hover:ease-out duration-150">
                     H
                   </p>
-                  <p className="group-hover:mx-0.5 hover:ease-out duration-150">
+                  <p className="md:group-hover:mx-0.5 md:hover:ease-out duration-150">
                     O
                   </p>
-                  <p className="group-hover:mx-0.5 hover:ease-out duration-150">
+                  <p className="md:group-hover:mx-0.5 md:hover:ease-out duration-150">
                     M
                   </p>
-                  <p className="group-hover:mx-0.5 hover:ease-out duration-150">
+                  <p className="md:group-hover:mx-0.5 md:hover:ease-out duration-150">
                     E
                   </p>
                 </span>
